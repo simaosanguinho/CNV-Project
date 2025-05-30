9d3a2b5c05eabe9cb4c3fb8848ea77d050d4d437
pereira0x <jose.a.pereira03@gmail.com>
2025-05-30 18:02:41 +0100

    fix: remove instance

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -4,10 +4,11 @@ source config.sh
 
 echo "=== CNV Project Full Termination ==="
 
-# Step 1: Create the AMI image
+echo "Stopping VM instance..."
+aws ec2 terminate-instances --instance-ids $(cat instance.id)
+
 echo "Stopping AMI image..."
 $DIR/deregister-image.sh
 
-# Step 2: Launch the deployment
 echo "Stopping deployment infrastructure..."
 $DIR/terminate-deployment-template.sh
